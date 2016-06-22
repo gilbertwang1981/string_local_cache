7846c6728a8d939074e987f2bd3b9cec6fba5ea7
gilbert.wang <gilbert.wang>
2016-06-22 09:46:30 +0800

    1

--- src/main/java/com/vip/local/cache/data/LocalCacheData.java
+++ src/main/java/com/vip/local/cache/data/LocalCacheData.java
@@ -1,11 +1,15 @@
 package com.vip.local.cache.data;
 
 import java.util.Map;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class LocalCacheData {
 	private Map<String , String> cache = null;
 	
 	private static LocalCacheData instance = null;
+	
+	private ReadWriteLock readwritelock = new ReentrantReadWriteLock();
 	
 	public static LocalCacheData getInstance(){
 		if (instance == null) {
@@ -16,14 +20,21 @@ public class LocalCacheData {
 	}
 	
 	public void switchCache(Map<String , String> cache) {
+		readwritelock.writeLock().lock();
 		this.cache = cache;
+		readwritelock.writeLock().unlock();
 	}
 	
 	public String get(String key) {
+		readwritelock.readLock().lock();
 		if (this.cache == null) {
+			readwritelock.readLock().unlock();
 			return null;
 		}
 		
-		return this.cache.get(key);
+		String ret = this.cache.get(key);
+		readwritelock.readLock().unlock();
+		
+		return ret;
 	}
 }
