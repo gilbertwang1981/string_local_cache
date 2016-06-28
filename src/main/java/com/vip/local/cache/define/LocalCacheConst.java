2b307cf665b68af77f6593bc29c7aa0184a168e8
gilbert.wang <gilbert.wang>
2016-06-28 14:03:27 +0800

    1

--- src/main/java/com/vip/local/cache/define/LocalCacheConst.java
+++ src/main/java/com/vip/local/cache/define/LocalCacheConst.java
@@ -2,7 +2,7 @@ package com.vip.local.cache.define;
 
 public enum LocalCacheConst {
 	LOCAL_CACHE_SERVER_PORT("10012" , "服务器端口"),
-	LOCAL_CACHE_MAX_FRAME_SIZE("102400" , "最大针大小"),
+	LOCAL_CACHE_MAX_FRAME_SIZE("2097152" , "最大针大小"),
 	LOCAL_CACHE_CMD_QUEUE_SIZE("819200" , "命令队列大小"),
 	LOCAL_CACHE_CMD_QUEUE_TMO("500" , "命令队列超时"),
 	LOCAL_CACHE_HC_TMO("1000" , "健康检查超时时间");
