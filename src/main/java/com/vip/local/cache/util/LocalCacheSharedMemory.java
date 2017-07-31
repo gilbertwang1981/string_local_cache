901016afeb938702b00e5383202eba96f59cfb9a
gilbert.wang <gilbert.wang@vipshop.com>
2017-07-31 10:14:13 +0800

    fix bugs

--- src/main/java/com/vip/local/cache/util/LocalCacheSharedMemory.java
+++ src/main/java/com/vip/local/cache/util/LocalCacheSharedMemory.java
@@ -1,7 +1,6 @@
 package com.vip.local.cache.util;
 
 import java.io.File;
-import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
@@ -9,8 +8,9 @@ import java.nio.channels.FileChannel;
 import com.vip.local.cache.proto.SharedMemoryStruct.SharedMemoryObject;
 
 public class LocalCacheSharedMemory {
-	private int totalRecored = 16;
-	private int pageFlag = 0;
+	private int totalRecored = 6;
+	private int rPage = 0;
+	private int wPage = 0;
 	private int writeOffset = 0;
 	private int writeCtr = 0;
 	private int readOffset = 0;
@@ -20,8 +20,14 @@ public class LocalCacheSharedMemory {
 	private FileChannel fileChannel = null;
 	private MappedByteBuffer mapBuffer = null;
 	
-	public boolean initialize(String fileName) {
+	private LocalCacheFileLock localCacheFileLock = new LocalCacheFileLock();
+	
+	private static final String LOCAL_CACHE_FILE_LOCK_NAME = "localcache.lock";
+	
+	public boolean initialize(String fileName) throws Exception {
 		try {
+			localCacheFileLock.lock(LOCAL_CACHE_FILE_LOCK_NAME);
+			
 			boolean isNew = false;
 			File file = new File(fileName);
 			if (!file.exists()) {
@@ -35,7 +41,8 @@ public class LocalCacheSharedMemory {
 			
 			if (isNew) {
 				mapBuffer.putInt(totalRecored);
-				mapBuffer.putInt(pageFlag);
+				mapBuffer.putInt(wPage);
+				mapBuffer.putInt(rPage);
 				mapBuffer.putInt(writeOffset);
 				mapBuffer.putInt(writeCtr);
 				mapBuffer.putInt(readOffset);
@@ -44,28 +51,34 @@ public class LocalCacheSharedMemory {
 			
 			mapBuffer.position(0);
 			this.totalRecored = mapBuffer.getInt();
-			this.pageFlag = mapBuffer.getInt();
+			this.wPage = mapBuffer.getInt();
+			this.rPage = mapBuffer.getInt();
 			this.writeOffset = mapBuffer.getInt();
 			this.writeCtr = mapBuffer.getInt();
 			this.readOffset = mapBuffer.getInt();
 			this.readCtr = mapBuffer.getInt();
+			
+			localCacheFileLock.unlock();
 		} catch (Exception e) {
+			localCacheFileLock.unlock();
 			return false;
 		}
 		
 		return true;
 	}
 	
-	public boolean write(SharedMemoryObject obj) throws IOException {
+	public boolean write(SharedMemoryObject obj) throws Exception {
+		localCacheFileLock.lock(LOCAL_CACHE_FILE_LOCK_NAME);
+		
 		byte [] in = obj.toBuilder().build().toByteArray();
 		
 		if (this.writeCtr >= this.totalRecored) {
-			this.pageFlag = 1;
+			this.wPage ++;
 			this.writeCtr = 0;
 			this.writeOffset = 0;
 		}		
 		
-		mapBuffer.position(24 + this.writeOffset);
+		mapBuffer.position(28 + this.writeOffset);
 		
 		mapBuffer.putInt(in.length);
 		mapBuffer.put(in);
@@ -74,26 +87,45 @@ public class LocalCacheSharedMemory {
 		this.writeOffset += in.length + 4;
 		
 		mapBuffer.position(4);
-		mapBuffer.putInt(pageFlag);
+		mapBuffer.putInt(wPage);
+		
+		mapBuffer.position(12);
 		mapBuffer.putInt(writeOffset);
 		mapBuffer.putInt(writeCtr);
+		
+		localCacheFileLock.unlock();
 		
 		return true;
 	}
 	
-	public SharedMemoryObject read() throws IOException {
+	public SharedMemoryObject read() throws Exception {
 		
-		if (this.pageFlag == 0 && this.readCtr >= this.writeCtr) {
+		localCacheFileLock.lock(LOCAL_CACHE_FILE_LOCK_NAME);
+		
+		mapBuffer.position(0);
+		this.totalRecored = mapBuffer.getInt();
+		this.wPage = mapBuffer.getInt();
+		this.rPage = mapBuffer.getInt();
+		this.writeOffset = mapBuffer.getInt();
+		this.writeCtr = mapBuffer.getInt();
+		this.readOffset = mapBuffer.getInt();
+		this.readCtr = mapBuffer.getInt();
+		
+		if (this.rPage == this.wPage && this.readCtr == this.writeCtr) {
+			System.out.println("write ctr:" + this.writeCtr + " read ctr:" + this.readCtr);
+		
+			localCacheFileLock.unlock();
+			
 			return null;
 		}
 		
-		if (this.readCtr >= this.totalRecored) {
-			this.readCtr = 0;
-			this.readOffset = 0;
-			this.pageFlag = 0;
+		if (this.rPage < this.wPage && this.readCtr == this.totalRecored) {
+				this.readCtr = 0;
+				this.readOffset = 0;
+				this.rPage ++;
 		}
 		
-		mapBuffer.position(24 + this.readOffset);
+		mapBuffer.position(28 + this.readOffset);
 		
 		int length = mapBuffer.getInt();
 		byte [] dst = new byte[length];
@@ -102,18 +134,22 @@ public class LocalCacheSharedMemory {
 		this.readCtr ++;
 		this.readOffset += dst.length + 4;
 		
-		mapBuffer.position(4);
-		mapBuffer.putInt(this.pageFlag);
+		mapBuffer.position(8);
+		mapBuffer.putInt(this.rPage);
 		
-		mapBuffer.position(16);
+		mapBuffer.position(20);
 		mapBuffer.putInt(this.readOffset);
 		mapBuffer.putInt(this.readCtr);
+		
+		localCacheFileLock.unlock();
 		
 		return SharedMemoryObject.parseFrom(dst);
 	}
 	
-	public boolean destroy(){
+	public boolean destroy() throws Exception{
 		try {
+			localCacheFileLock.lock(LOCAL_CACHE_FILE_LOCK_NAME);
+			
 			if (fileChannel != null) {
 				fileChannel.close();
 			}
@@ -124,8 +160,12 @@ public class LocalCacheSharedMemory {
 			
 			mapBuffer = null;
 			
+			localCacheFileLock.unlock();
+			
 			return true;
 		} catch (Exception e) {
+			localCacheFileLock.unlock();
+			
 			return false;
 		}
 	}
--- src/test/java/com/vip/local/cache/test/InterProcessShmTest.java
+++ src/test/java/com/vip/local/cache/test/InterProcessShmTest.java
@@ -6,26 +6,36 @@ import com.vip.local.cache.util.LocalCacheSharedMemory;
 
 import junit.framework.TestCase;
 
-public class SharedMemoryTest extends TestCase {
-	public void testSharedMemory() throws Exception{
+public class InterProcessShmTest extends TestCase {
+	public void testProducer() throws Exception{
 		LocalCacheSharedMemory shm = new LocalCacheSharedMemory();
 		
-		shm.initialize("test0.shm");
+		shm.initialize("test1.shm");
 		
-		for (int i = 0;i < 5;i ++) {
+		for (int i = 0;i < 60;i ++) {
 			SharedMemoryObject obj = SharedMemoryStruct.SharedMemoryObject.newBuilder().setIp("127.0.0.1")
 					.setKey("test-key").setValue("hello world" + i).setTimestamp(System.currentTimeMillis()).build();
 			shm.write(obj);
+			
+			Thread.sleep(1000);
 		}
+		
+		shm.destroy();
+	}
+
+	public void testConsumer() throws Exception {
+		LocalCacheSharedMemory shm = new LocalCacheSharedMemory();
+		
+		shm.initialize("test1.shm");
 		
 		while (true) {
 			SharedMemoryObject out = shm.read();
 			if (out == null) {
-				break;
+				Thread.sleep(3000);
+				continue;
 			}
+			
 			System.out.println("output:" + out.getIp() + " " + out.getKey() + " " + out.getValue() + " " + out.getTimestamp());
 		}
-			
-		shm.destroy();
 	}
 }
