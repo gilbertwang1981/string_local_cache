485c6a63627d3adebb7caf48fd5502e44c6c31a8
gilbert.wang <gilbert.wang@vipshop.com>
2017-08-09 15:12:18 +0800

    enlarge the max file index

--- src/main/java/com/vip/local/cache/db/DBShmConst.java
+++ src/main/java/com/vip/local/cache/db/DBShmConst.java
@@ -2,6 +2,6 @@ package com.vip.local.cache.db;
 
 public interface DBShmConst {
 	public static final int DB_SHM_SIZE_IN_EACH_DB = 1024;
-	public static final int DB_SHM_SIZE_IN_DBS = 1024;
+	public static final int DB_SHM_SIZE_IN_DBS = 16384;
 	public static final int DB_SHM_MAX_SIZE_IN_EACH_ELEM = 8192;
 }
