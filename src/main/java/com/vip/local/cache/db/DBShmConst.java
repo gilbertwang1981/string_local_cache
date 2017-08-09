3b4e8d1c54c121aac6ae25b998cb1d298c900ddb
gilbert.wang <gilbert.wang@vipshop.com>
2017-08-09 15:09:21 +0800

    modify the db default size, the size of each cache object is 8k. in each file, there are 1024 cache object. there are 1024 db files.

--- src/main/java/com/vip/local/cache/db/DBShmConst.java
+++ src/main/java/com/vip/local/cache/db/DBShmConst.java
@@ -1,7 +1,7 @@
 package com.vip.local.cache.db;
 
 public interface DBShmConst {
-	public static final int DB_SHM_SIZE_IN_EACH_DB = 32;
-	public static final int DB_SHM_SIZE_IN_DBS = 32;
-	public static final int DB_SHM_MAX_SIZE_IN_EACH_ELEM = 4096;
+	public static final int DB_SHM_SIZE_IN_EACH_DB = 1024;
+	public static final int DB_SHM_SIZE_IN_DBS = 1024;
+	public static final int DB_SHM_MAX_SIZE_IN_EACH_ELEM = 8192;
 }
